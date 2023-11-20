60869c4a371682eaac0e03c7874fb21c8d817fab
User <user.name>
2023-11-20 03:01:26 -0300

    remove cache graphQL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,17 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const wordpressApi = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore'
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all'
+    }
+  }
 })
 
 export { wordpressApi }
